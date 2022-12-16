28e4ba94cbea04e3555e1e6958a5a105ae581428
KP <thao8615@stthomas.edu>
2022-12-15 21:37:47 -0600

    Updated Map Code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import $ from 'jquery'
+// import L from 'leaflet'
 
 export default {
     data() {
@@ -8,6 +9,9 @@ export default {
             codes: [],
             neighborhoods: [],
             incidents: [],
+            address: '',
+            map: null,
+            geojsonLayer: null,
             leaflet: {
                 map: null,
                 center: {
@@ -39,7 +43,7 @@ export default {
                     {location: [44.937705, -93.136997], marker: null},
                     {location: [44.949203, -93.093739], marker: null}
                 ]
-            }
+            },
         };
     },
     methods: {
@@ -53,12 +57,6 @@ export default {
 
         viewAbout(event) {
             this.view = 'about';
-        },
-        mapChange(event){
-            this.panTo([44.955139, -93.102222])
-            
-            this.leaflet.center.lat= 44.955139;
-            this.leaflet.center.lng = -93.102222;
         },
 
         getJSON(url) {
@@ -92,8 +90,54 @@ export default {
                     }
                 });
             });
+        },
+        async search(){
+            // Try parsing the search input as longitude and latitude coordinates
+            const coords = this.parseCoords(this.searchInput)
+            //checks if coordinates can be parsed
+            if(coords) {
+                // Check if the coordinates are within the bounds
+                if (!(this.leaflet.center.lat >= 44.883658 && this.leaflet.center.lat <= 45.008206 && this.leaflet.center.lng >= -93.217977 && this.leaflet.center.lng <= -92.993787)) {
+                    console.alert('Coordinates are not within bounds. Please enter something that is within bounds.');
+                    return;
+                }
+                this.panTo(coords);
+                return;
+            }
+            try {
+                const res = await this.geocode(this.searchInput)
+                if(res.length > 0) {
+                    const coords = res[0].center
+                    this.panTo(res[0].center)
+                
+                    // Check if the coordinates are within the GeoJSON layer
+                    if(!(this.geojsonlayer.getBounds().contains(coords))){
+                        console.alert("The address you submitted is not within the GeoJSON layer")
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        },
+        //parses coordinates
+        parseCoords(input){
+            const matches = input.match(/^\s*(-?\d+(?:\.\d+)?),\s*(-?\d+(?:\.\d+)?)\s*$/)
+            if(matches){
+                return [parseFloat(matches[1], parseFloat(matches[2]))]
+            }
+            return null
+        },
+        //takes an input string and returns the latitude and logitude coodinations for location
+        async geocode(input){
+            const response = await fetch('/data/StPaulDistrictCouncil.geojson')
+            return response.json()
+        },
+        //pans to location on map
+        panTo(coords){
+            this.map.panTo(coords)
         }
     },
+    //Sets up Leaflet map and adds it to the page. Also adds tile layer and GeoJSON data to the map.
     mounted() {
         this.leaflet.map = L.map('leafletmap').setView([this.leaflet.center.lat, this.leaflet.center.lng], this.leaflet.zoom);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -116,7 +160,6 @@ export default {
         });
     }
 }
-
 </script>
 
 <template>
@@ -129,147 +172,36 @@ export default {
     </div>
     <div v-show="view === 'map'">
         <div class="grid-container">
+            <div>
+                <input type="text" v-model="searchInput" />
+                <button @click="search">Search</button>
+            </div>
             <div class="grid-x grid-padding-x">
                 <div id="leafletmap" class="cell auto"></div>
             </div>
-                    <!-- This is the stuff for making the 'incidents' table underneath the map-->
-            <div class="grid-x grid-padding-x">
-                <button type="button" @click="mapChange">TEST</button>
-                <div>____</div>
-                <button type="button">TEST2</button>
-                <div>____</div>
-                <p @click="mapChange">TEST3</p>
-                <table>
-                    <tr>
-                        <th>Item</th>
-                        <th>Case Number</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Code</th>
-                        <th>Incident</th>
-                        <th>Police-Grid</th>
-                        <th>Neighborhood</th>
-                        <th>Block</th>
-                    </tr>
-                    <tr v-for="index in 1000" :key="index">
-                        <td> {{index}} #</td>
-                        <td>CASE</td>
-                        <td>DATE</td>
-                        <td>TIME</td>
-                        <td>CODE</td>
-                        <td>INCIDENT</td>
-                        <td>POLICE-Grid</td>
-                        <td>NEIGHBORHOOD</td>
-                        <td>BLOCK</td>
-
-
-                    </tr>
-                </table>
-            </div>
         </div>
-    
     </div>
-
-    <!-- FORM INPUT STUFF-->
     <div v-if="view === 'new_incident'">
         <!-- Replace this with your actual form: can be done here or by making a new component -->
         <div class="grid-container">
             <div class="grid-x grid-padding-x">
-                <h1 class="cell small-12 medium-12 large-12">New Incident Form</h1>
+                <h1 class="cell auto">New Incident Form</h1>
             </div>
         </div>
-        <form>
-            <div class="grid-container">
-                <div class="grid-x grid-padding-x">
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="case">Case Number:</label>
-                        <input type="text" id="case" name="case">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Date">Date (YYYY-MM-DD):</label>
-                        <input type="text" id="Date" name="Date">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Time">Time (HR:MM:SS):</label>
-                        <input type="text" id="Time" name="Time">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Code">Code:</label>
-                        <input type="text" id="Code" name="Code">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Incident">Incident</label>
-                        <input type="text" id="Incident" name="Incident">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="PGrid">Police Grid</label>
-                        <input type="text" id="PGrid" name="PGrid">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Neighbor">Neighborhood Name:</label>
-                        <input type="text" id="Neighbor" name="Neighbor">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <label for="Street">Block/Street:</label>
-                        <input type="text" id="Street" name="Street">
-                    </div>
-                    <div class="cell small-12 medium-6 large-4">
-                        <input type="button" value="SUBMIT">
-                    </div>
-                </div>
-            </div>
-        </form>
-
-
     </div>
-
-
-
-
     <div v-if="view === 'about'">
         <!-- Replace this with your actual about the project content: can be done here or by making a new component -->
-        <!-- 
-            Promisve.all([getJSON(...), getJSON(...), getJSON(...)]) makes it so the .then does not trigger until they are all done 
-            .then((results)=>{})
-            .catch((err)=>{});
-            Ideal for the crime API we need to get data from. 
-            results will be a list, results[0] will be the first promise, 
-            results[1] will be the data with the second promise, etc...
-
-        this.getJSON('/data/StPaulDistrictCouncil.geojson').then((result) => {
-            // St. Paul GeoJSON
-            $(result.features).each((key, value) => {
-                district_boundary.addData(value);
-            });
-        }).catch((error) => {
-            console.log('Error:', error);
-        });
-
-        Code from up above
-            
-        -->
         <div class="grid-container">
             <div class="grid-x grid-padding-x">
-                <h1 class="cell small-12 medium-12 large-12">About the Project</h1>
-                
-                <div class ="cell small-12 medium-4 large-4" >
-                    <h3>Kong Pheng Thao</h3>
-                </div>
-                <div class = "cell small-12 medium-4 large-4">
-                    <h3>Michael Andreucci</h3>
-                </div>
-                <div class="cell small-12 medium-4 large-4">
-                    <h3>Andrew Steichen</h3>
-                </div>
+                <h1 class="cell auto">About the Project</h1>
             </div>
-
         </div>
     </div>
 </template>
 
 <style>
 #leafletmap {
-    height: 500px;
+    height: 600px;
 }
 
 .selected {
@@ -285,12 +217,6 @@ export default {
     border: solid 1px white;
     text-align: center;
     cursor: pointer;
-}
-table, th, td{
-    text-align:center;
-    color: black;
-    border: 1px solid black;
-    background-color:darkgrey;
 }
 
 </style>
